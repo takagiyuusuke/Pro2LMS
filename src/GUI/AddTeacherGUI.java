bd6655c0c2469168709a79f506e7bf8b57a0aaf4
harukyabe2 <kakarotto.3662.dragon@gmail.com>
2024-07-05 10:54:48 +0900

    AddTeacherGUIの中身変更

--- src/GUI/AddTeacherGUI.java
+++ src/GUI/AddTeacherGUI.java
@@ -1,25 +1,16 @@
 package GUI;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 import database.TeacherDB;
 
 import java.awt.*;
 import java.awt.event.*;
-
 public class AddTeacherGUI {
 	
 	private JFrame frame = new JFrame("Add Teacher");
 	private String name;
 	private int teacherId;
-	private boolean nameIsOk = false;
-	private boolean teacherIdIsOk = false;
-	private JLabel errlabel = new JLabel("");
-	private JLabel errlabel2 = new JLabel("");
-	private JTextField nameField = new JTextField("");
-	private JTextField teacherIdField = new JTextField("");
 	private JButton button = new JButton("Add Teacher");
 	private TeacherDB teacherDB;
 	private boolean frameClosed = false;
@@ -32,29 +23,33 @@ public class AddTeacherGUI {
 	private void initUI() {
 		this.button.setEnabled(false);
 		
-		this.nameField.getDocument().addDocumentListener(this.listener);
-		this.teacherIdField.getDocument().addDocumentListener(this.listener);
-		
 		ButtonAction buttonListener = new ButtonAction();
 		this.button.addActionListener(buttonListener);
 		
-		JPanel namePane = new JPanel();
-		namePane.setLayout(new GridLayout(1, 0));
-		namePane.add(new JLabel("Enter Name:"));
-		namePane.add(this.nameField);
-		
-		JPanel teacherIdPane = new JPanel();
-		teacherIdPane.setLayout(new GridLayout(1, 0));
-		teacherIdPane.add(new JLabel("Enter Teacher ID:"));
-		teacherIdPane.add(this.teacherIdField);
+		NameField namePane = new NameField("Enter Name");
+		IdField idPane = new IdField("Enter Teacher ID");
+		namePane.setOnTextChanged((String s) -> {
+			this.name = s;
+			if (namePane.isOk() && idPane.isOk()) {
+				button.setEnabled(true);
+			} else {
+				button.setEnabled(false);
+			}
+		});
+		idPane.setOnTextChanged((Integer id) -> {
+			this.teacherId = id;
+			if (namePane.isOk() && idPane.isOk()) {
+				button.setEnabled(true);
+			} else {
+				button.setEnabled(false);
+			}
+		});
 		
 		JPanel pane = new JPanel();
 		pane.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 		pane.setLayout(new GridLayout(0, 1));
 		pane.add(namePane);
-		pane.add(this.errlabel);
-		pane.add(teacherIdPane);
-		pane.add(this.errlabel2);
+		pane.add(idPane);
 		pane.add(this.button);
 		
 		this.frame.getContentPane().add(pane, BorderLayout.CENTER);
@@ -80,77 +75,6 @@ public class AddTeacherGUI {
 			frame.dispose();
 		}
 	}
-
-	private void nameCheck() {
-		String newName = nameField.getText();
-		if (newName.length() == 0) {
-			this.errlabel.setText("name should have at least 1 letter!");
-			this.errlabel.setForeground(Color.red);
-			this.nameIsOk = false;
-		} else if (newName.length() >= 20) {
-			this.errlabel.setText("name should be shorter than 20 letters!");
-			this.errlabel.setForeground(Color.red);
-			this.nameIsOk = false;
-		} else if (!newName.matches("[a-zA-Z\\s]*")) {
-			this.errlabel.setText("name should have only alphabets and space!");
-			this.errlabel.setForeground(Color.red);
-			this.nameIsOk = false;
-		} else {
-			this.errlabel.setText("there are no problem with name!");
-			this.errlabel.setForeground(Color.blue);
-			this.name = newName;
-			this.nameIsOk = true;
-		}
-	}
-
-	private void teacherIdCheck() {
-		String newidString = this.teacherIdField.getText();
-		int newid;
-		try {
-			newid = Integer.valueOf(newidString);
-		} catch (Exception ex) {
-			this.errlabel2.setText("teacher ID should have numbers!");
-			this.errlabel2.setForeground(Color.red);
-			this.teacherIdIsOk = false;
-			return;
-		}
-		if (newidString.length() != 6) {
-			this.errlabel2.setText("teacher ID should have 6 digits!");
-			this.errlabel2.setForeground(Color.red);
-			this.teacherIdIsOk = false;
-		} else {
-			this.errlabel2.setText("there are no problem with teacher ID!");
-			this.errlabel2.setForeground(Color.blue);
-			this.teacherId = newid;
-			this.teacherIdIsOk = true;
-		}
-	}
-
-	class TextFieldDocumentListener implements DocumentListener {
-		@Override
-		public void insertUpdate(DocumentEvent e) {
-			onTextChanged(e);
-		}
-
-		@Override
-		public void removeUpdate(DocumentEvent e) {
-			onTextChanged(e);
-		}
-
-		@Override
-		public void changedUpdate(DocumentEvent e) {
-			onTextChanged(e);
-		}
- 
-		private void onTextChanged(DocumentEvent e) {
-			nameCheck();
-			teacherIdCheck();
-			if (nameIsOk && teacherIdIsOk) button.setEnabled(true);
-			else button.setEnabled(false);
-		}
-	}
-
-	TextFieldDocumentListener listener = new TextFieldDocumentListener();
 
 	public synchronized void waitForClose() throws InterruptedException {
 		while (!this.frameClosed) {
